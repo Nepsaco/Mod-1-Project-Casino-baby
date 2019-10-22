a1a1d098790278f2efb69e84dacd397c175d9acf
github username <github email address>
2019-10-22 12:55:30 -0600

    leaderboards

--- config/environment.rb
+++ config/environment.rb
@@ -2,29 +2,9 @@ require 'sinatra/activerecord'
 require 'sqlite3'
 require 'require_all'
 require 'pry'
+require 'rest-client' #might not need 
 
 require_all 'lib' 
 
-def Welcome
-	puts "Welcome!"
-	puts "logo"
-end
 
-def main_menu
-	puts 'Pick an option'
-	puts '1. login'
-	puts '2. sign up'
-	puts '3. exit'
-	user_input = gets.strip
-	case user_input
-		when 1
-		 go to login page
-		when 2
- 		 go to sign up page
-		when 3
-		 exit game
-	end
-end
-binding.pry
-0 
 
---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+tobie = User.create(user_name: "Tobie", balance: 2000)
+adam = User.create(user_name: "Adam", balance: 1500)
+amy = User.create(user_name: "Amy", balance: 250)
+
