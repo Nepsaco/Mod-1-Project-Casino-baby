4dc780f2e6b0df4f9821aed253c6d7edeba278b0
Nepsaco <tobie.tsuzuki@gmail.com>
2019-10-24 15:58:37 -0600

    added mf pictures

--- config/environment.rb
+++ config/environment.rb
@@ -13,7 +13,9 @@ ActiveRecord::Base.logger = nil
 shuffle = RestClient.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6')
 shuffle_j = JSON.parse(shuffle)
 deck_id = shuffle_j["deck_id"]
-draw_a_card = RestClient.get("https://deckofcardsapi.com/api/deck/#{deck_id}/draw/?count=2")
+draw_a_card = RestClient.get("https://deckofcardsapi.com/api/deck/#{deck_id}/draw/?count=1")
 draw_cards_j = JSON.parse(draw_a_card)
 reshuffle = RestClient.get("https://deckofcardsapi.com/api/deck/#{deck_id}/shuffle/")
 reshuffle_j = JSON.parse(reshuffle) 
+
+
